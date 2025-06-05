795ed09ffcf22a03d5f6d6fe773b1f0599d20fd0
Wayne Ngan <wayne.ngan@gmail.com>
2025-06-05 05:06:07 -0400

    add yoyosam

--- sponsors.js
+++ sponsors.js
@@ -53,13 +53,14 @@ function loadOneGoldSponsor(num, elem, scale) {
         case 4:
             showOneSponsor(elem, "http://magicyoyo.cn/", "sponsors/magicyoyo.png", "MagicYoYo", 400*scale);
             break;
+        case 5:
+            showOneSponsor(elem, "https://www.yoyosam.com/", "sponsors/yoyosam.png", "YoYoSam", 310*scale);
+            break;
 
         // case 3:
             // showOneSponsor(elem, "https://www.c3yoyodesign.com/", "sponsors/c3yoyodesign.png", "C3yoyodesign", 400*scale);
             // break;
-        // case 4:
-            // showOneSponsor(elem, "https://www.yoyosam.com/", "sponsors/yoyosam.png", "YoYoSam", 300*scale);
-            // break;
+        // 
         // case 5:
             // showOneSponsor(elem, "https://truearcreturntops.ca/", "sponsors/TrueArc.png", "True Arc Return Tops", 300*scale);
             // break;
@@ -89,7 +90,7 @@ function loadSilverSponsors(id, scale) {
 function loadGoldSponsors(id, scale) {
     elem = $("#"+id);
     
-    numSponsors = 5;
+    numSponsors = 6;
 
     order = genRandArray(numSponsors);
     for (i = 0; i < numSponsors; i++) {
